8d8386857fb547bfddd1322e65d8c28dc395d5f0
PhilippLgh <philipp@ethereum.org>
2020-06-05 19:01:03 +0200

    fix example

--- examples/start_stop_client.js
+++ examples/start_stop_client.js
@@ -1,11 +1,8 @@
-const { ClientManager } = require('../dist/index.js')
+const { getClient } = require('../dist/index.js')
 
 const run = async () => {
-  const cm = new ClientManager()
-  console.log('state 1', await cm.status())
-  const clientInfo = await cm.startClient('geth', 'latest', ['--goerli'])
-  console.log('state 2', await cm.status())
-  const result = await cm.stopClient(clientInfo.id)
-  console.log('state 3', await cm.status())
+  const geth = await getClient('geth')
+  await geth.start('--goerli')
+  await geth.stop()
 }
 run()
