54624518115e36a8779804eb4f5a861ddc9f22d9
PhilippLgh <philipp@ethereum.org>
2020-06-05 15:36:50 +0200

    add ethpkg re-export + constants

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import { MultiClientManager, SingleClientManager, getClient } from './ClientManager'
+import { MultiClientManager, SingleClientManager, getClient, SHARED_DATA } from './ClientManager'
 export { SingleClientManager, MultiClientManager, getClient }
 
 export * from './types'
 export { CLIENT_STATE } from './Client/BaseClient'
 
+export { PROCESS_EVENTS } from './events'
+
+export * as ethpkg from 'ethpkg'
+
+export { SHARED_DATA }
 
 const instance = MultiClientManager.getInstance()
 export default instance
