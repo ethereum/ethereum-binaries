6d8a255eae2bd7288d90808757c996df909fb3a9
PhilippLgh <philipp@ethereum.org>
2020-06-05 15:28:57 +0200

    improve zokrates example

--- examples/zokrates/zokrates.js
+++ examples/zokrates/zokrates.js
@@ -12,16 +12,18 @@ const log = async prom => {
 // see https://zokrates.github.io/gettingstarted.html#hello-zokrates
 const run = async () => {
   const zokrates = await getClient('zokrates')
-  fs.writeFileSync(path.join(__dirname, 'test.zok'),   `
+  fs.writeFileSync(path.join(__dirname, 'test.zok'), `
   def main(private field a, field b) -> (field):
     field result = if a * a == b then 1 else 0 fi
     return result
   `)
-  await log( zokrates.execute(`compile -i ${SHARED_DATA}/test.zok`) )
-  await log( zokrates.execute(`setup`) )
-  await log( zokrates.execute('compute-witness -a 337 113569') )
-  await log( zokrates.execute('generate-proof') )
-  await log( zokrates.execute(`export-verifier`) )
-  await log( zokrates.execute(`cp ./verifier.sol ${SHARED_DATA}`, { useBash: true, useEntrypoint: false }))
+  await log(zokrates.execute(`compile -i ${SHARED_DATA}/test.zok`))
+  await log(zokrates.execute(`setup`))
+  await log(zokrates.execute('compute-witness -a 337 113569'))
+  await log(zokrates.execute('generate-proof'))
+  await log(zokrates.execute(`export-verifier`))
+  await log(zokrates.execute(`cp ./verifier.sol ${SHARED_DATA}`, { useBash: true, useEntrypoint: false }))
+
+  await zokrates.stop()
 }
 run()
