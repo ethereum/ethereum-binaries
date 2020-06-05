c63e93cb61ec2971996e0a7520c248147ee69ec5
PhilippLgh <philipp@ethereum.org>
2020-06-05 18:58:51 +0200

    fix create account example

--- examples/create_account.js
+++ examples/create_account.js
@@ -1,9 +1,9 @@
-const { ClientManager } = require('../dist/index.js')
+const { SingleClientManager } = require('../dist/index.js')
 
 const run = async () => {
-  const cm = new ClientManager()
-  const client = await cm.getClient('geth', 'latest')
-  const result = await cm.execute(client, 'account new', {
+  const cm = new SingleClientManager()
+  const client = await cm.getClient('geth')
+  const result = await cm.execute('account new', {
     timeout: 20 * 1000, // user has 20 seonds to enter password
     stdio: 'inherit' // NOTE: without 'inherit' "account new" will expect stdin input for password and will always time out
   })
